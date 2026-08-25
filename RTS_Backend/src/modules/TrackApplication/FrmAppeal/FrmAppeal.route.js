36ba9f1587bac22d89b7f7a3935cb28e5dea5e3e
rishi-ascentech <rishi.dubey@ascentechindia.com>
2026-08-25 10:46:21 +0530

    FrmAppeal backend + frontend completed

--- RTS_Backend/src/modules/TrackApplication/FrmAppeal/FrmAppeal.route.js
+++ RTS_Backend/src/modules/TrackApplication/FrmAppeal/FrmAppeal.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const controller = require("./FrmAppeal.controller");
 const auth = require("../../../middlewares/auth.middleware");
 
-router.post("/raise-appeal", controller.raiseAppealController);
-router.get("/application-details", controller.getApplicationDetailsController);
+router.post("/raise-appeal", auth(), controller.raiseAppealController);
+router.get("/application-details", auth(), controller.getApplicationDetailsController);
 
 module.exports = router;
