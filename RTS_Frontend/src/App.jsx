76d30e95d2f9c84ce5ac42151e4bf82bf8470f77
rishi-ascentech <rishi.dubey@ascentechindia.com>
2026-08-28 11:43:58 +0530

    FrmServiceApplicationMst frontend

--- RTS_Frontend/src/App.jsx
+++ RTS_Frontend/src/App.jsx
@@ -25,6 +25,7 @@ import FrmFirstAppeal from "./Pages/FrmFirstAppeal";
 import FrmFirstAppealDocUpload from "./Pages/FrmFirstAppealDocUpload";
 import FrmMarketLicenseUpdt from "./Pages/Trade/FrmMarketLicenseupdt";
 import FrmMarriageRgstn from "./Pages/Marriage/FrmMarriageRgstn";
+import FrmServiceApplicationMst from "./Pages/SolidHealth/FrmServiceApplicationMst";
 
 const router = createBrowserRouter([
   {
@@ -136,6 +137,11 @@ const router = createBrowserRouter([
         path: "/app/FrmMarriageRgstn",
         element: <FrmMarriageRgstn />
       },
+       {
+        path: "/app/FrmServiceApplicationMst",
+        element: <FrmServiceApplicationMst />,
+      },
+
     ]
   },
 
