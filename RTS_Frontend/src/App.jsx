dedd444f3cb471d4e5c6227f2f0269c5179c649c
kartikdeore3887 <kartik.deore@ascentechindia.com>
2026-08-28 12:34:57 +0530

    FrmServiceApplicationMstNew and API implementation

--- RTS_Frontend/src/App.jsx
+++ RTS_Frontend/src/App.jsx
@@ -25,6 +25,7 @@ import FrmFirstAppeal from "./Pages/FrmFirstAppeal";
 import FrmFirstAppealDocUpload from "./Pages/FrmFirstAppealDocUpload";
 import FrmMarketLicenseUpdt from "./Pages/Trade/FrmMarketLicenseupdt";
 import FrmMarriageRgstn from "./Pages/Marriage/FrmMarriageRgstn";
+import FrmServiceApplicationMstNew from "./Pages/PWD/FrmServiceApplicationMstNew";
 
 const router = createBrowserRouter([
   {
@@ -135,6 +136,12 @@ const router = createBrowserRouter([
       {
         path: "/app/FrmMarriageRgstn",
         element: <FrmMarriageRgstn />
+      },
+
+
+      {
+        path: "/app/FrmServiceApplicationMstNew",
+        element: <FrmServiceApplicationMstNew />
       },
     ]
   },
