2c232fc6d94a41219691bf249e8eeb10565331e2
Kascentech <kavita.upadhyay@ascentechindia.com>
2026-09-04 15:32:29 +0530

    tradetype

--- RTS_Backend/src/app.js
+++ RTS_Backend/src/app.js
@@ -121,7 +121,8 @@ app.use("/api/FrmDeptMappingConfig", require("./modules/AdminDepartment/FrmDeptM
 
 //AFrmAppAutherisation
 app.use("/api/frmAppAuth", require("./modules/AdminDepartment/FrmAppAuth/frmAppAuth.routes"));
-app.use("/api/Doclist",require("./modules/AdminDepartment/FrmDocList/frmDocList.routes"))
+app.use("/api/Doclist",require("./modules/AdminDepartment/FrmDocList/frmDocList.routes"));
+app.use("/api/Tradetypeconfig", require("./modules/AdminDepartment/FrmTradeCtgryTypeCnfgList/frmTradeCtgryTypeCnfgList.routes"))
 
 
 
